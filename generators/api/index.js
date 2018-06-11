e674462033809eb1eafa189f45cee5bf575262a9
Suhendra Ahmad <syuaibi@gmail.com>
2018-06-12 03:04:31 +0800

    Add fool proof index injection for router.use and require()

--- generators/api/index.js
+++ generators/api/index.js
@@ -87,22 +87,22 @@ module.exports = class extends Generator {
     /**
      * Controller
      */
-    copyTpl(tPath('_controller.js'), dPath(`src/api/controllers/${controllerName}`), props);
+    //copyTpl(tPath('_controller.js'), dPath(`src/api/controllers/${controllerName}`), props);
 
     /**
      * Route
      */
-    copyTpl(tPath('_route.js'), dPath(`src/api/routes/${this.apiversion}/${routeName}`), props);
+    //copyTpl(tPath('_route.js'), dPath(`src/api/routes/${this.apiversion}/${routeName}`), props);
 
     /**
      * Validation
      */
-    copyTpl(tPath('_validation.js'), dPath(`src/api/validations/${validationName}`), props);
+    //copyTpl(tPath('_validation.js'), dPath(`src/api/validations/${validationName}`), props);
 
     /**
      * Integration Test
      */
-    copyTpl(tPath('_test.js'), dPath(`src/api/tests/integration/${testName}`), props);
+    //copyTpl(tPath('_test.js'), dPath(`src/api/tests/integration/${testName}`), props);
 
     //done();
   }
@@ -114,7 +114,7 @@ module.exports = class extends Generator {
 
     const name = props.name.toLowerCase();
     const apigroup = props.apigroup.toLowerCase();
-    const routeName = `${apigroup}.route.js`;
+    const routeName = `${apigroup}.route`;
 
     /**
      * Inject codes
@@ -127,40 +127,79 @@ module.exports = class extends Generator {
       var ast = recast.parse(this.fs.read(routesFile));
       var body = ast.program.body;
 
+      var injectRequire = true;
+      var injectRouterUse = true;
+
       /**
        * Route require injection to index.js
        */
-      var lastImportIndex = _.findLastIndex(body, function(statement) {  
-        return statement.type === 'VariableDeclaration';
-      });
-      var actualImportCode = recast.print(body[lastImportIndex]).code;
-      var importString = ['const ', apigroup, 'Routes = require(\'./', routeName, '\');'].join('');
-      body.splice(lastImportIndex, 1, importString);
-      body.splice(lastImportIndex, 0, actualImportCode);
+      recast.visit(ast, {
+        visitVariableDeclaration: function(path) {
+          
+          const declarations = path.node.declarations
 
+          // Check for const = require variable declaration
+          if (declarations.length > 0) {
+            let decl = declarations[0]
+            var arg = decl.init.arguments.length > 0 ? decl.init.arguments[0].value : '';
+
+            if (decl.id.name.trim() === `${apigroup}Routes` && arg.trim() === `./${routeName}`) {
+              // We found that const route = require('') already exist, so prevent from re-injecting
+              injectRequire = false;        
+            }
+          }          
+          this.traverse(path);
+        },
+
+        visitCallExpression: function(path) {
+          const callee = path.node.callee;
+          if (callee.type === 'MemberExpression') {
+            const args = path.node.arguments;
+            const validFirstArgument = args.length > 0 && args[0].type === 'Literal' && args[0].value === `/${apigroup}`;
+            const validSecondArgument = args.length > 1 && args[1].type === 'Identifier' && args[1].name === `${apigroup}Routes`;
+            const isValid = callee.object.name === 'router' && callee.property.name === 'use' && validFirstArgument && validSecondArgument;
+            injectRouterUse = !isValid
+          }
+          this.traverse(path);
+        }
+      });
+
+      // Inject const = require()
+      if (injectRequire) {
+        var lastImportIndex = _.findLastIndex(body, function(statement) {
+          return statement.type === 'VariableDeclaration';
+        });
+        var actualImportCode = recast.print(body[lastImportIndex]).code;
+        var importString = ['const ', apigroup, 'Routes = require(\'./', routeName, '\');'].join('');
+        body.splice(lastImportIndex < 0 ? 0 : lastImportIndex, 1, importString);
+        body.splice(lastImportIndex < 0 ? 0 : lastImportIndex, 0, actualImportCode);
+      }
+      
       /**
        * Inject router.use() expression
        */
-      var middlewareString = ['router.use(\'/', apigroup, '\', ', apigroup, 'Routes);'].join('');
-
-      var lastMiddlewareIndex = _.findLastIndex(body, function (statement) {
-        if (!statement.expression || !statement.expression.callee) {
-          return false;
-        }
-        var callee = statement.expression.callee;
-        return callee.object.name === 'router' && callee.property.name === 'use';
-      });
-
-      
-      if (lastMiddlewareIndex === -1) {
-        var exportRouterIndex = _.findIndex(body, function (statement) {
-          return statement.type === 'ExportDefaultDeclaration';
+      if (injectRouterUse) {
+        var middlewareString = ['router.use(\'/', apigroup, '\', ', apigroup, 'Routes);'].join('');
+        var lastMiddlewareIndex = _.findLastIndex(body, function (statement) {
+          if (!statement.expression || !statement.expression.callee) {
+            return false;
+          }
+          var callee = statement.expression.callee;
+          console.log("calee", statement.expression.arguments);
+          return callee.object.name === 'router' && callee.property.name === 'use';
         });
-        body.splice(exportRouterIndex, 0, middlewareString);
-      } else {
-        var actualMiddlewareCode = recast.print(body[lastMiddlewareIndex]).code;
-        body.splice(lastMiddlewareIndex, 1, middlewareString);
-        body.splice(lastMiddlewareIndex, 0, actualMiddlewareCode);
+
+        
+        if (lastMiddlewareIndex === -1) {
+          var exportRouterIndex = _.findIndex(body, function (statement) {
+            return statement.type === 'ExportDefaultDeclaration';
+          });
+          body.splice(exportRouterIndex < 0 ? 0 : exportRouterIndex, 0, middlewareString);
+        } else {
+          var actualMiddlewareCode = recast.print(body[lastMiddlewareIndex]).code;
+          body.splice(lastMiddlewareIndex < 0 ? 0 : lastMiddlewareIndex, 1, middlewareString);
+          body.splice(lastMiddlewareIndex < 0 ? 0 : lastMiddlewareIndex, 0, actualMiddlewareCode);
+        }
       }
 
       rimraf(routesFile, () => {
