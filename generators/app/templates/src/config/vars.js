2ada40ffc68e1d1c955df79a49f1d19ca6174bfd
Suhendra Ahmad <syuaibi@gmail.com>
2018-10-25 21:47:07 +0800

    Fixed api integration test and moved dotenv loader from vars to index

--- generators/app/templates/src/config/vars.js
+++ generators/app/templates/src/config/vars.js
@@ -1,11 +1,3 @@
-const path = require('path');
-
-// import .env variables
-require('dotenv-safe').load({
-  path: path.join(__dirname, '../../.env'),
-  sample: path.join(__dirname, '../../.env.example')
-});
-
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
