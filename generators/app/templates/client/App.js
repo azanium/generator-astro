dc7211804a877d7cf57a0c78dd865a2199fd0c40
Suhendra Ahmad <syuaibi@gmail.com>
2019-01-20 23:31:19 +0800

    Finished react + webpack + hmr (alpha)

---
+++ generators/app/templates/client/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './app.css';
+import ReactImage from './react.png';
+
+export default class App extends Component {
+  state = { username: null };
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
+
+  render() {
+    const { username } = this.state;
+    return (
+      <div>
+        {username ? <h1>{`Hello This ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <img src={ReactImage} alt="react" />
+        <img src="/images/quran.jpg" alt="react" />
+      </div>
+    );
+  }
+}
