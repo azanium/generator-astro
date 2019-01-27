139ecd53b86a0ab7b37b9d85ef5238deaff09f7c
Suhendra Ahmad <syuaibi@gmail.com>
2019-01-27 19:27:48 +0800

    Add module alias for client and components and fixed start script

--- generators/app/templates/reza.config.js
+++ generators/app/templates/reza.config.js
@@ -3,13 +3,15 @@ const path = require('path');
 module.exports = {
   modify: (config) => {
     config.resolve.alias = {  // eslint-disable-line
-      '@app': path.resolve('src/server'),
-      '@config': path.resolve('src/server/config'),
-      '@api': path.resolve('src/server/api'),
-      '@middlewares': path.resolve('src/server/middlewares'),
-      '@models': path.resolve('src/server/models'),
-      '@services': path.resolve('src/server/services'),
-      '@utils': path.resolve('src/server/utils')
+      '@app': path.resolve('<%= src %>/server'),
+      '@config': path.resolve('<%= src %>/config'),
+      '@api': path.resolve('<%= src %>/api'),
+      '@middlewares': path.resolve('<%= src %>/middlewares'),
+      '@models': path.resolve('<%= src %>/models'),
+      '@services': path.resolve('<%= src %>/services'),
+      '@utils': path.resolve('<%= src %>/utils'),
+      '@client': path.resolve('<%= client %>'),
+      '@components': path.resolve('<%= client %>/components')
     };
     return config;
   }
